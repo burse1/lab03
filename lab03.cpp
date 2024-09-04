dbcaaa9bf1d956fe445db83691aac543ee103f01
burse1 <spencerburse@gmail.com>
2024-09-04 13:41:06 -0400

    Add project files.

---
+++ lab03.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+void printMenu(int& choice) {
+	cout << "Welcome to my program, please select an operation to perform:" << endl;
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Division" << endl;
+	cout << "\nYour Selection: ";
+	cin >> choice;
+}
+void getChoices(float& A, float& B) {
+	cout << "Please enter the first value: ";
+	cin >> A;
+	cout << "Please enter the second value: ";
+	cin >> B;
+	// The rest of this function is an exercise to the reader
+}
+void firstChoice(float A, float B) {
+	float C;
+	cout << "Addition" << endl << endl;
+	C = A + B;
+
+	cout << A << " + " << B << " = " << C;
+}
+void secondChoice(float A, float B) {
+	float C;
+	cout << "Subtraction" << endl << endl;
+	C = A - B;
+
+	cout << A << " - " << B << " = " << C;
+
+}
+void thirdChoice(float A, float B) {
+	float C;
+	cout << "Multiplication" << endl << endl;
+	C = A * B;
+
+	cout << A << " * " << B << " = " << C;
+
+}
+void fourthChoice(float A, float B) {
+	float C;
+	cout << "Division" << endl << endl;
+	C = A / B;
+
+	cout << A << " / " << B << " = " << C;
+
+}
+int main() {
+	int choice;
+	float A;
+	float B;
+	printMenu(choice);
+	getChoices(A, B);
+	if (choice == 1) {
+		firstChoice(A, B);
+	}
+	if (choice == 2) {
+		secondChoice(A, B);
+	}
+	if (choice == 3) {
+		thirdChoice(A, B);
+	}
+	if (choice == 4) {
+		fourthChoice(A, B);
+	}
+
+	return 0;
+}
